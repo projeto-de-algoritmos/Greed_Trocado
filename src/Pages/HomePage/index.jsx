a80b166fda66f2ac747c0e5c4b84713416fceeb0
AlineLermen <aline.helena.lermen@gmail.com>
2021-09-01 16:59:04 -0300

    Passa os valores para realizar cálculos

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,13 +1,31 @@
+import React from "react";
 import "./style.css";
 import { useHistory } from "react-router-dom";
 
 function HomePage() {
+  const [total, setTotal] = React.useState();
+  const [pago, setPago] = React.useState();
   const history = useHistory();
 
-  function handleSubmit() {
-    // alert('Um nome foi enviado:');
-    history.push({ pathname: "/materiator" });
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    history.push({ 
+      pathname: "/materiator",
+      state: { 
+        total: total,
+        pago: pago
+      },
+    });
   }
+
+  const handleChangeTotal = (event) => {
+    setTotal(event.target.value);
+  };
+
+  const handleChangePago = (event) => {
+    setPago(event.target.value);
+  };
 
   return (
     <div className="HomePage">
@@ -20,20 +38,21 @@ function HomePage() {
       <br />
       <br />
 
-      <form  onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <label className="label">
           Valor total (R$):
           <br />
-          <input className="textField" type="number" name="total" />
+          <input className="textField" type="number" value={total} onChange={handleChangeTotal}/>
           <br />
           <br />
           Valor pago (R$):
           <br />
-          <input className="textField" type="number" name="pago" />
+          <input className="textField" type="number" value={pago} onChange={handleChangePago}/>
           <br />
         </label>
         <input className="calculateButton" type="submit" value="Calcular"/>
       </form>
+
     </div>
   );
 }
--- src/Pages/Materiator/index.js
+++ src/Pages/Materiator/index.js
@@ -1,10 +1,18 @@
 import "./style.css";
 import MSelector from "../../Components/MateriaSelector/MSelector";
 
-function Materiator() {
+function Materiator(history) {
+
+  let total = history.location.state.total;
+  let pago = history.location.state.pago;
+
+  console.log("total", total)
+  console.log("pago", pago)
+
   return (
     <>
       <h1 className="question"> Quais matérias c já fez?</h1>
+
 
       <MSelector></MSelector>
     </>
